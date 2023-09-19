855f6b131a882f6ab0d7f495641b18f1a7d75a19
profi200 <fd3194@gmx.de>
2023-09-19 20:58:03 +0200

    Added a few missing copyright headers.

--- source/arm11/config.c
+++ source/arm11/config.c
@@ -1,3 +1,21 @@
+/*
+ *   This file is part of open_agb_firm
+ *   Copyright (C) 2023 profi200
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include "types.h"
@@ -8,7 +26,7 @@
 
 
 #define INI_BUF_SIZE    (1024u)
-// Note: Keep this synchronized with g_oafConfig.
+// Note: Keep this synchronized with g_oafConfig in open_agb_firm.c.
 #define DEFAULT_CONFIG  "[general]\n"             \
                         "backlight=64\n"          \
                         "backlightSteps=5\n"      \
